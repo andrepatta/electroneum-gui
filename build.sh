c824f65218a14dff4d4fd345f22425a752c8a59e
andrepatta <andre_luis@outlook.com>
2018-12-02 00:49:36 -0200

    Build GUI from electroneum repository

--- build.sh
+++ build.sh
@@ -45,8 +45,8 @@ fi
 source ./utils.sh
 pushd $(pwd)
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-MONERO_DIR=monero
-MONEROD_EXEC=monerod
+ELECTRONEUM_DIR=electroneum
+ELECTRONEUMD_EXEC=electroneumd
 
 MAKE='make'
 if [[ $platform == *bsd* ]]; then
@@ -73,13 +73,13 @@ fi
 if [ "$platform" == "darwin" ]; then
     BIN_PATH=$BIN_PATH/monero-wallet-gui.app/Contents/MacOS/
 elif [ "$platform" == "mingw64" ] || [ "$platform" == "mingw32" ]; then
-    MONEROD_EXEC=monerod.exe
+    ELECTRONEUMD_EXEC=electroneumd.exe
 fi
 
 # force version update
 get_tag
 echo "var GUI_VERSION = \"$TAGNAME\"" > version.js
-pushd "$MONERO_DIR"
+pushd "$ELECTRONEUM_DIR"
 get_tag
 popd
 echo "var GUI_MONERO_VERSION = \"$TAGNAME\"" >> version.js
@@ -90,7 +90,7 @@ $MAKE || exit
 
 # Copy monerod to bin folder
 if [ "$platform" != "mingw32" ] && [ "$ANDROID" != true ]; then
-cp ../$MONERO_DIR/bin/$MONEROD_EXEC $BIN_PATH
+cp ../$ELECTRONEUM_DIR/bin/$ELECTRONEUMD_EXEC $BIN_PATH
 fi
 
 # make deploy
